93cbf8483247b79433aa2294d504db914d3778f3
EldenBear <dontet1995@gmail.com>
2023-05-08 09:08:15 -0400

    added role values

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,16 @@
-INSERT INTO department (id , department_name)
+INSERT INTO department(id, department_name)
+VALUES
+    (1, "Product Development"),
+    (2, "Sales"),
+    (3, "IT");
+
+INSERT INTO role (id, title, salary, department_id)
 VALUES 
-    (1, "Team Lead"), 
-    (2, "Principal Engineer"),
-    (3, "Senior Engineer"),
-    (4, "Software Engineer"),
-    (5, "Junior Engineer");
+    (1, "Team Lead", 150000, 1), 
+    (2, "Principal Engineer", 130000, 1),
+    (3, "Senior Engineer", 110000, 1),
+    (4, "Software Engineer", 90000, 1),
+    (5, "Junior Engineer", 70000, 1),
+    (6, "Sales Consultant", 40000, 2),
+    (7, "IT Technician", 50000, 3);
 
