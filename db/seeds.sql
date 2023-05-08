61e6d4ecd74f41a3cc9ca0ea22de11960f3ccd51
EldenBear <dontet1995@gmail.com>
2023-05-08 09:21:49 -0400

    added values for employee seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -14,3 +14,13 @@ VALUES
     (6, "Sales Consultant", 40000, 2),
     (7, "IT Technician", 50000, 3);
 
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES
+    (1, "Phil", "Brooks", 1, null),
+    (2, "Tina", "Grey", 2, 1),
+    (3, "Kobe", "Bryant", 3, 1),
+    (4, "John", "Jacob", 4, 1),
+    (5, "Kristen", "Apple", 4, 1),
+    (6, "Mark", "Calloway", 5, 1),
+    (7, "Goblin", "Man", 6, null),
+    (8, "Glasses", "MaHoney", 7, null);
