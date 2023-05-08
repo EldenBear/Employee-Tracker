3a8509ddcff77bba460d0b1474f209260f6c5390
EldenBear <dontet1995@gmail.com>
2023-05-08 14:47:27 -0400

    installed express and set up skeleton for server.js

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const { default: Choices } = require('inquirer/lib/objects/choices');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'employees_db'
+  },
+  console.log(`Connected to the employees_db database.`)
+);
+
+const viewAllDepartments = "View all departments";
+const viewAllRoles = "View all roles";
+const viewAllEmployees = "View all employees";
+const addDepartment = "Add a department";
+const addRole = "Add a role";
+const addEmployee = "Add an employee";
+const updateEmployeeRole = "Update employee role";
+
+const questions = [
+    {
+      type: "list",
+      name: "directory",
+      message: "What would you like to do?",
+      choices:[viewAllDepartments, viewAllRoles, viewAllEmployees, addDepartment, addRole, addEmployee, updateEmployeeRole] 
+    },
+  ];
+
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+ 
+ inquirer.prompt(questions).then((data) => {
+    if (data.directory === viewAllDepartments){}
+  });
+
+});
